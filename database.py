d2d3679c43b51581ee6bbea67ddf838c5f151540
louis <louis.morin.pro@gmail.com>
2019-11-12 17:12:18 -0500

    Adding marshmallow seralization

--- database.py
+++ database.py
@@ -17,6 +17,16 @@ class User(db.Model, UserMixin):
     bday = db.Column(db.DateTime)    
     estate = db.relationship('Estate', backref="owner_estate", cascade="delete", lazy='dynamic')
 
+class UserSchema(ma.ModelSchema):
+    class Meta:
+        id = fields.Int(dump_only=True)
+        surname = fields.Str()
+        name = fields.Str()
+        bday = fiels.DateTime()
+        estate = fields.Nested(EstateSchema, many=True, only=["name"])
+
+
+
 class Estate(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     id_owner=db.Column(db.Integer, db.ForeignKey("user.id"))
@@ -26,8 +36,24 @@ class Estate(db.Model):
     city = db.Column(db.Text)
     rooms = db.relationship('Room', backref="rl_room", cascade="delete", lazy='dynamic')
 
+class EstateSchema(ma.TableSchema):
+    class Meta:
+        id = fields.Int(dump_only=True)
+        description = fields.Str()
+        name = fields.Str()
+        re_type = fields.Str()
+        bday = fiels.DateTime()
+        city = fields.Str()
+        rooms = fields.Nested(RoomSchema, many=True)
+        id_owner = fields.Int()
+
 class Room(db.Model):
     id=db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
     description = db.Column(db.Text)
     id_estate=db.Column(db.Integer, db.ForeignKey("estate.id"))
+
+class RoomSchema(ma.TableSchema):
+    class Meta:
+        id = fields.Int(dump_only=True)
+        description = fields.Str()
